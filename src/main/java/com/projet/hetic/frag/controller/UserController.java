576ee4f87df26ec38fe9ef0475c78f05aac8fcaa
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2025-02-17 16:44:42 +0100

    feat(controller): Add example user controller

---
+++ src/main/java/com/projet/hetic/frag/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.projet.hetic.frag.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/")
+  public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+    return userService.getUserById(id)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
+}
