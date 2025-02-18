dff0c8a6960cd12a4673b3bdce0af8eb7038a040
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2025-02-18 22:28:15 +0100

    feat(Chunk): Add chunk service

---
+++ src/main/java/com/projet/hetic/frag/service/ChunkService.java
@@ -0,0 +1,21 @@
+package com.projet.hetic.frag.service;
+
+import org.springframework.stereotype.Service;
+
+import com.projet.hetic.frag.model.Chunk;
+
+@Service
+public class ChunkService {
+
+  public Chunk createChunk(byte[] bytes) {
+    Chunk chunk = new Chunk();
+
+    // get original size
+    // get compressed sized
+    // get data
+    // get hash
+
+    return chunk;
+  }
+
+}
