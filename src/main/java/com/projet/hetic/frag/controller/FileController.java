33c2e2894df301caa5178a694685f7f2daaf4139
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-02-18 12:01:41 +0100

    chore: Update dependencies and improve Swagger configuration

--- src/main/java/com/projet/hetic/frag/controller/FileController.java
+++ src/main/java/com/projet/hetic/frag/controller/FileController.java
@@ -1,15 +1,20 @@
 package com.projet.hetic.frag.controller;
 
 import com.projet.hetic.frag.dto.FileResponseDTO;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/files")
 @RequiredArgsConstructor
+@Tag(name = "Files", description = "File management endpoints")
 public class FileController {
+    @Operation(summary = "Split a file into chunks")
     @PostMapping("/split")
     public ResponseEntity<String> splitFile(@RequestParam("file") MultipartFile file) {
         return ResponseEntity.ok(file.getOriginalFilename());
@@ -24,6 +29,4 @@ public class FileController {
     public ResponseEntity<String> unsplitFile(@PathVariable String fileId) {
         return ResponseEntity.ok("supp");
     }
-
-
 }
