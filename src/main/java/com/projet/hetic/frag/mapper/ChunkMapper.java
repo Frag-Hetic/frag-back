f908d1b93e1c7d5537b40d7bed3e43271c5f0b1a
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2025-02-18 22:34:49 +0100

    feat(Chunk): Add chunk mapper

--- src/main/java/com/projet/hetic/frag/mapper/ChunkMapper.java
+++ src/main/java/com/projet/hetic/frag/mapper/ChunkMapper.java
@@ -2,14 +2,22 @@ package com.projet.hetic.frag.mapper;
 
 import org.springframework.stereotype.Component;
 
+import com.projet.hetic.frag.model.Chunk;
+
 @Component
 public class ChunkMapper {
-//    public ChunkDTO toDto(Chunk chunk) {
-//        return new ChunkDTO(
-//                chunk.getId(),
-//                chunk.getHash(),
-//                chunk.getSizeOriginal(),
-//                chunk.getSizeCompressed()
-//        );
-//    }
+  // public ChunkDTO toDto(Chunk chunk) {
+  // return new ChunkDTO(
+  // chunk.getId(),
+  // chunk.getHash(),
+  // chunk.getSizeOriginal(),
+  // chunk.getSizeCompressed()
+  // );
+  // }
+  public Chunk bytesToEntity(byte[] bytes) {
+    Chunk chunk = new Chunk();
+    chunk.setSizeOriginal(bytes.length);
+    chunk.setData(bytes);
+    return chunk;
+  }
 }
--- src/main/java/com/projet/hetic/frag/service/ChunkService.java
+++ src/main/java/com/projet/hetic/frag/service/ChunkService.java
@@ -2,17 +2,21 @@ package com.projet.hetic.frag.service;
 
 import org.springframework.stereotype.Service;
 
+import com.projet.hetic.frag.mapper.ChunkMapper;
 import com.projet.hetic.frag.model.Chunk;
 
 @Service
 public class ChunkService {
+  private final ChunkMapper chunkMapper;
+
+  public ChunkService(ChunkMapper chunkMapper) {
+    this.chunkMapper = chunkMapper;
+  }
 
   public Chunk createChunk(byte[] bytes) {
-    Chunk chunk = new Chunk();
+    Chunk chunk = chunkMapper.bytesToEntity(bytes);
 
-    // get original size
     // get compressed sized
-    // get data
     // get hash
 
     return chunk;
