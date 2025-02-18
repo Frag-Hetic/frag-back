18d5c92454df632a16c487f99fbf355451ffb6c0
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-02-18 15:56:52 +0100

    test(setup): environment & first tests with mock

--- src/test/java/com/projet/hetic/frag/FragApplicationTests.java
+++ src/test/java/com/projet/hetic/frag/FragApplicationTests.java
@@ -2,12 +2,17 @@ package com.projet.hetic.frag;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1"
+})
+@ActiveProfiles("test")
 class FragApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
