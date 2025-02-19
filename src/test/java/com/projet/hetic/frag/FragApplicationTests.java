e6f42c147d854153edeb4fed2be4293b51fb1d56
Maxence Nho <78363268+PiKwaii93@users.noreply.github.com>
2025-02-19 14:00:07 +0100

    fix(conflict)

--- src/test/java/com/projet/hetic/frag/FragApplicationTests.java
+++ src/test/java/com/projet/hetic/frag/FragApplicationTests.java
@@ -1,13 +1,18 @@
 package com.projet.hetic.frag;
 
 import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-public class FragApplicationTests {
+@SpringBootTest(properties = {
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1"
+})
+@ActiveProfiles("test")
+class FragApplicationTest {
 
-    //@SpringBootTest
     @Test
     void contextLoads() {
-        // Test ignoré
     }
 }
