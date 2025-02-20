34025a93f2167307508af676a9c9b54d82a0adeb
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2025-02-21 00:28:59 +0100

    test(Service): Add test on file reconstruction processor service

--- src/main/java/com/projet/hetic/frag/exception/FileProcessingException.java
+++ src/main/java/com/projet/hetic/frag/exception/FileProcessingException.java
@@ -2,7 +2,7 @@ package com.projet.hetic.frag.exception;
 
 public class FileProcessingException extends RuntimeException {
   public FileProcessingException(String error) {
-    super(String.format("Error reading file: %s", error));
+    super(String.format("Error processing file: %s", error));
   }
 
 }
